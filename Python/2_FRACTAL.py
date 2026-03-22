e84a96685ed312737abc6dc65186d1806f6d0f26
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-22 14:34:50 +0500

    Update 2_FRACTAL.py

--- Python/2_FRACTAL.py
+++ Python/2_FRACTAL.py
@@ -17,3 +17,27 @@ def draw_amazing_fractal():
     
     hue = 0.0
     
+    for i in range(400):
+        color = colorsys.hsv_to_rgb(hue, 0.8, 1)
+        t.pencolor(color)
+        hue += 0.003
+        
+        t.forward(i)
+        t.right(100) 
+        
+        if i % 2 == 0:
+            t.circle(i * 0.5, 90) 
+            t.right(70)
+        else:
+            t.circle(i * 0.2, -90)
+            t.left(70)
+            
+        if i % 2 == 0:
+            screen.update()
+
+    screen.update()
+    print("Amazing Design Complete!")
+    screen.exitonclick()
+
+if __name__ == "__main__":
+    draw_amazing_fractal()
