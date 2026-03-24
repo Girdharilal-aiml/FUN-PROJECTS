13460b6b22f07a007fafd952efa344adadb4127b
Girdhari lal <girdharilal.aiml@gmail.com>
2026-03-24 16:55:27 +0500

    Update 3_PINK_HEART.py

--- Python/3_PINK_HEART.py
+++ Python/3_PINK_HEART.py
@@ -7,3 +7,13 @@ screen.setup(800, 800)
 screen.bgcolor("black")
 screen.title("Amazing Neon Heart Animation")
 
+t = turtle.Turtle()
+t.speed(1)  
+t.pensize(2)
+t.hideturtle()
+
+def heart_x(k):
+    return 15 * math.sin(k)**3
+
+def heart_y(k):
+    return 12 * math.cos(k) - 5 * math.cos(2*k) - 2 * math.cos(3*k) - math.cos(4*k)
