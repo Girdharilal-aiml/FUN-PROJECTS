9620cc4e15164ddd6721f2c2a95bf13b281fae64
Girdhari lal <girdharilal.aiml@gmail.com>
2026-04-10 23:30:23 +0500

    Update 11_FLUID_TREE.py

--- Python/11_FLUID_TREE.py
+++ Python/11_FLUID_TREE.py
@@ -5,3 +5,4 @@ def setup_canvas():
     screen = turtle.Screen()
     screen.bgcolor("#F5F5F0") 
     screen.setup(width=900, height=900)
+    screen.title("Fluid Recursive Growth (Biological Ink)")
